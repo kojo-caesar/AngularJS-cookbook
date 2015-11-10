8bdee038fa18f632d692c518f13497468211f078
kojo <kojo.caesar@gmail.com>
2015-11-10 15:29:36 +0000

    added comments

---
+++ blacklist-validator/app.js
@@ -0,0 +1,31 @@
+angular.module('blacklistVal', [])
+    .directive('blacklist', function ($parse) {
+        return {
+            require:'ngModel',
+            link: function (scope, element, attrs, NgModelController) {
+                // badWords takes the blacklistValues provided in $scope by MyCtrl controller
+                // $parse returns a function
+                var badWords = $parse(attrs.blacklist)(scope) || [];
+                console.log("badWords = " + badWords);
+                console.log("NgModelController BEFORE = " + NgModelController);
+                console.log("NgModelController.$parsers BEFORE = " + NgModelController.$parsers);
+                
+                // Functions added to $parsers are called as soon as the value in the form input is modified by the user
+                NgModelController.$parsers.push(function (value) {
+                    // console.log(value);
+                    if (value) {
+                        var containsBadWord = badWords.some(function(str) {
+                           return value.indexOf(str) >= 0; 
+                        });
+                        console.log("containsBadWord = " + containsBadWord);
+                        NgModelController.$setValidity('blacklist', !containsBadWord);
+                    }
+                });
+                console.log("NgModelController.$parsers AFTER = " + NgModelController.$parsers);
+                console.log("NgModelController AFTER = " + NgModelController);
+            }
+        };
+    })
+    .controller('MyCtrl', function($scope) {
+       $scope.blacklistValues = ['hello', 'bye', "Hi"]; 
+    });
