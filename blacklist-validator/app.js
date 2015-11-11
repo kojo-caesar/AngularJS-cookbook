7881959bf1617b00a73a6a37d549340d817753f0
kojo <kojo_caesar@yahoo.com>
2015-11-11 13:45:20 +0000

    new functionality: badwords load from json

--- blacklist-validator/app.js
+++ blacklist-validator/app.js
@@ -1,14 +1,36 @@
-angular.module('blacklistVal', [])
+angular
+    .module('blacklistVal', [])
+    .controller('MyCtrl', ['$scope', '$http', function($scope, $http) {
+       $scope.blacklistValues = ['Ola'];
+       //console.log(typeof $scope.blacklistValues, $scope.blacklistValues);
+       
+       $http.get('blacklist.json').success(function(data) {
+           var arr = $scope.blacklistValues;
+           for(var x = 0; x < data['blacklistV'].length; x++){
+               arr.push(data['blacklistV'][x]);
+           }
+           $scope.blacklistValues = arr;
+           //console.log(typeof $scope.blacklistValues, $scope.blacklistValues);
+       });
+    }])
     .directive('blacklist', function ($parse) {
         return {
             require:'ngModel',
             link: function (scope, element, attrs, NgModelController) {
                 // badWords takes the blacklistValues provided in $scope by MyCtrl controller
                 // $parse returns a function
-                var badWords = $parse(attrs.blacklist)(scope) || [];
-                console.log("badWords = " + badWords);
-                console.log("NgModelController BEFORE = " + NgModelController);
-                console.log("NgModelController.$parsers BEFORE = " + NgModelController.$parsers);
+                // var badWords = $parse(attrs.blacklist)(scope) || [];
+                var badWords;
+                
+                scope.$watch(function () {
+                    return scope[attrs.blacklist];
+                }, function (newValue, oldValue) {
+                    badWords = newValue || [];
+                });
+                
+                //console.log("badWords = " + badWords);
+                //console.log("NgModelController BEFORE = " + NgModelController);
+                //console.log("NgModelController.$parsers BEFORE = " + NgModelController.$parsers);
                 
                 // Functions added to $parsers are called as soon as the value in the form input is modified by the user
                 NgModelController.$parsers.push(function (value) {
@@ -22,11 +44,8 @@ angular.module('blacklistVal', [])
                         NgModelController.$setValidity('blacklist', !containsBadWord);
                     }
                 });
-                console.log("NgModelController.$parsers AFTER = " + NgModelController.$parsers);
-                console.log("NgModelController AFTER = " + NgModelController);
+                //console.log("NgModelController.$parsers AFTER = " + NgModelController.$parsers);
+               // console.log("NgModelController AFTER = " + NgModelController);
             }
         };
-    })
-    .controller('MyCtrl', function($scope) {
-       $scope.blacklistValues = ['hello', 'bye', "Hi"]; 
     });
