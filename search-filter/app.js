614d1ea768ffd39304f06634f9e136081fdca55b
kojo <kojo_caesar@yahoo.com>
2015-11-24 14:47:23 +0000

    color highlight when filtering search

---
+++ search-filter/app.js
@@ -0,0 +1,13 @@
+angular.module("search-filter", ['ngSanitize'])
+    .filter("highlight", function() {
+        return function(input, search) {
+            if (search === '') return input;
+            else {
+                var searchRegExp = new RegExp('(' + search + ')','gi');
+                return input.replace(searchRegExp, '<span class="highlight">$1</span>');
+            }
+        };
+    })
+    .controller("MainCtrl", function($scope) {
+        $scope.items = ['Ceas', "Caine", "Laptop", "Bec"];
+    });
