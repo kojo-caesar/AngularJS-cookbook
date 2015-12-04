3a02bb6cf2ccaabb3f98e16183ce112c5ef8ac15
kojo <kojo_caesar@yahoo.com>
2015-12-04 09:54:21 +0000

    adding cache-data-w-promise

---
+++ cache-data-w-promise/app.js
@@ -0,0 +1,24 @@
+angular.module("myApp", [])
+    .controller("myCtrl", function($scope, $http, $q) {
+        var cache;
+        
+        function userCache() {
+            if(cache) {
+                console.log("Loaded data from cache");
+                return $q.when(cache);
+            }
+            else {
+                console.log("Loaded data from webserver");
+                return $http.get("users.json").then(function(response) {
+                    cache = response.data;
+                    return response.data;
+                });
+            }
+        }
+        $scope.loadUsers = function() {
+            userCache().then(function(users) {
+                $scope.users = users;
+            });
+        };
+        
+    });
